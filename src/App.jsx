afdd7d9b2727a94e52fffdffef35fe1046b0d360
Giuseppe <cresctelli@gmail.com>
2025-10-14 12:47:11 +0200

    Added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import { Toaster } from "react-hot-toast";
+import { AnimatePresence, motion } from "motion/react";
 
 import { WorkoutsHistory } from "./pages/WorkoutsHistory";
 import { Workout } from "./pages/Workout";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 
+function AnimatedRoutes() {
+  const location = useLocation();
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route
+          index
+          element={
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Home />
+            </motion.div>
+          }
+        />
+
+        <Route path="app" element={<WorkoutsHistory />} />
+        <Route path="workout" element={<Workout />} />
+        <Route
+          path="login"
+          element={
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <Login />
+            </motion.div>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Toaster />
-        <Routes>
-          <Route index element={<Home />} />
-
-          <Route path="app" element={<WorkoutsHistory />} />
-          <Route path="workout" element={<Workout />} />
-          <Route path="login" element={<Login />} />
-        </Routes>
-      </div>
+      <Toaster />
+      <AnimatedRoutes />
     </BrowserRouter>
   );
 }
